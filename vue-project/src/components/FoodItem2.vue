e670e0dacc27935b5d08f7f46d3534eec0590138
apccmfamador <cmfamador@student.apc.edu.ph>
2025-01-30 22:25:29 +0800

    added new vue file

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -17,11 +17,11 @@
 </script>
   
 <style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
+    .mango {
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightyellow;
+    } 
 </style>
