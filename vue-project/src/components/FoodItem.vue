10817ab09473533da14e970accf946e2bbeac61d
apccmfamador <cmfamador@student.apc.edu.ph>
2025-01-30 22:02:05 +0800

    fix

--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-        <h2>{{ name }}</h2>
-        <p>{{ message }}</p>
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
     </div>
-</template>
-
-<script>
-    export default {
-        data() {
-            return {
-                name: 'Apples'
-                message: 'I Like Apples.'
-            }
-        }
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Apples',
+        message: 'I like apples'
+      }
     }
-</script>
-
-<style></style>
+  };
+  </script>
+  
+  <style></style>
