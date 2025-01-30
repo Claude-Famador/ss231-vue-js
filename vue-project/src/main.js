003f795417fc66f921011957bd29638615f97eda
apccmfamador <cmfamador@student.apc.edu.ph>
2025-01-30 23:40:28 +0800

    fixed main.js

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,12 +1,7 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-import PersonalInfo from './components/PersonalInfo.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 
 const app = createApp(App)
-app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
-app.component('personal-info', PersonalInfo)
+app.component('personal-profile', PersonalProfile)
 app.mount('#app')
