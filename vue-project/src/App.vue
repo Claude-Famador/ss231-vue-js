d83106461de37a6fec137b50580f6aa00dea9418
apccmfamador <cmfamador@student.apc.edu.ph>
2025-02-10 21:34:48 +0800

    fixed and added vue components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,39 +1,14 @@
-<template>
-  <div class="app-container">
-    <h1>Personal Info</h1>
-    <personal-info/>
-    <comment-form>
-  </div>
 
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
+<template>
+  <h1>Food</h1>
+  <country/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
 
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
-
-  const countries = ref([])
-
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
-
-<style>
-.app-container {
-  padding: 20px;
-}
-
-h1 {
-  text-align: center;
-  color: #333;
-  margin-bottom: 30px;
-}
-</style>
+<script></script>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Comment from './components/Comment.vue'
+import FoodItem from './components/FoodItem.vue'
+import Country from './components/Country.vue'
 import CommentForm from './components/CommentForm.vue'
-import PersonalInfo from './components/PersonalInfo.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
-app.component('personal-info', PersonalInfo)
-app.component('comment', Comment)
+app.component('food-item', FoodItem)
+app.component('country', Country)
 app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
